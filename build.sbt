b4f8675e9088d51e523f8fddf15e492e4e4c8521
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-11-21 01:49:39 -0200

    Add date parsing plus option checker

--- build.sbt
+++ build.sbt
@@ -3,4 +3,6 @@ name := "streamparser"
 version := "1.0"
 
 scalaVersion := "2.12.0"
+
+libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "2.14.0"
     
---
+++ src/main/scala-2.12/io/github/silvaren/quoteparser/Main.scala
@@ -0,0 +1,50 @@
+package io.github.silvaren.quoteparser
+
+import com.github.nscala_time.time.Imports._
+
+import scala.util.matching.Regex
+
+object Main {
+  val formatter = DateTimeFormat.forPattern("yyyyMMdd").withZone(DateTimeZone.forID("America/Sao_Paulo"))
+
+  def priceParser(strLine: String, integerStart: Int): BigDecimal = {
+    val priceStr = strLine.substring(integerStart, integerStart + 11) + '.' +
+      strLine.substring(integerStart + 11, integerStart + 13)
+    BigDecimal(priceStr)
+  }
+
+  def dateParser(strLine: String, dateStart: Int): DateTime = {
+    val parsedDate = formatter.parseDateTime(strLine.substring(dateStart,dateStart + 8))
+    parsedDate.withHour(18)
+  }
+
+  def isOption(symbol: String): Boolean = {
+    val symbolPrefix = symbol.substring(0, 4)
+    //matches all symbols with up to four letters, one A to X letter and at least one digit at the end.
+    val optionMatcher = s"ˆ${symbolPrefix}[A-X][\\d]+$$".r
+    optionMatcher.findFirstIn(symbol).isDefined
+  }
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner (System.in)
+
+    sc.nextLine() // skip first line
+
+    val quote = sc.nextLine()
+    val stockSymbol = quote.substring(12,24).trim
+    val date = dateParser(quote, 2)
+    val closePrice = priceParser(quote, 108)
+    val highPrice = priceParser(quote, 69)
+    val lowPrice = priceParser(quote, 82)
+    val openPrice = priceParser(quote, 56)
+    val tradedVolume = quote.substring(152, 170).toInt
+    val trades = quote.substring(147, 152).toInt
+
+    if (isOption(stockSymbol)) {
+    }
+    else {
+    }
+    println(stockSymbol, date, openPrice, highPrice, lowPrice, closePrice, tradedVolume, trades)
+  }
+
+}
